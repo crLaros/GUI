6a1ba1099234d55e8dd470e0e638ced508cb47b9
crLaros <115877605+crLaros@users.noreply.github.com>
2023-05-01 19:15:51 +0200

    Update Exodus.lua

--- Exodus.lua
+++ Exodus.lua
@@ -5,7 +5,6 @@
 repeat task.wait() until game:IsLoaded()
 
 -- Localization
-local Game_Name = "nil"
 local fromRGB = Color3.fromRGB
 local newUDim2 = UDim2.new
 local newVector2 = Vector2.new
@@ -4541,7 +4540,7 @@ function library:Load(options)
     utility.format(options)
 
     utility.defaults(options, {
-        title = Game_Name,
+        title = "Made by crLaros",
         theme = "Default",
         overrides = {},
         folder = "exodus",
